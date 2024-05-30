789914a41758a1368ca0fe3aae42c63bcb516c0d
Vincenzo Fabrizio Viola <vincy1621@gmail.com>
2024-05-30 15:50:20 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<script setup>
+// import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <div>
+    <h1>Test Site</h1>
+
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Commodi neque dolor, laborum quibusdam id voluptatum suscipit cum quisquam, natus ea vero distinctio illo nostrum assumenda iusto aperiam alias. Qui, eius.</p>
+
+    <a href="">link test</a>
+  </div>
+</template>
+
+<style scoped lang="scss">
+
+@use './styles/variables.scss' as *;
+
+div {
+  h1 {
+    color: $primary;
+  }
+  
+  a {
+    color: $primary;
+
+  }
+}
+</style>
